5ec5e2a5a7bdf65f7c22ddea077eb5b751a292cb
Mayank <mayankbudhiraja88@gmail.com>
2024-12-23 10:59:55 +0530

    Started working on tailwind concepts

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { UtilityClasses } from "./basic_fundamentals/UtilityClasses";
+
+function App() {
+  return (
+    <>
+      <UtilityClasses />
+    </>
+  );
+}
+
+export default App;
---
+++ src/basic_fundamentals/UtilityClasses.jsx
@@ -0,0 +1,12 @@
+export const UtilityClasses = () => {
+  return (
+    <div className="bg-black rounded-xl shadow-lg flex items-center gap-x-4">
+      <div>
+        <div className="text-xl font-medium text-white">
+          Utility Fundamentals
+        </div>
+        <p className="text-slate-400">writing css using utility classes</p>
+      </div>
+    </div>
+  );
+};
